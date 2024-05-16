e5a3c3c9c1b9cb5963cc043726251abdb27e6910
Nadasheikh4 <147923829+Nadasheikh4@users.noreply.github.com>
2024-05-16 08:47:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log(square(5)); // 25
+console.log(square(10)); // 100
+//Alternatively, you can also use the exponentiation operator (**) 
+//to calculate the square of a number:
+function square(x) {
+    return Math.pow(x, 2);
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+console.log(square(5)); // 25
+console.log(square(10)); // 100
+
+
+//Alternatively, you can also use the exponentiation operator (**) 
+//to calculate the square of a number:
+
+
+function square(x) {
+  return x ** 2;
+}
